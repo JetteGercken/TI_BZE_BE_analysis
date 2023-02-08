b781262a5b77b1f0f1392a438c353db955aea5dc
JetteGercken <henriette.gercken@posteo.de>
2023-02-08 14:13:09 +0100

    library, wd

--- sripts/MoMoK/MoMoK_trees.R
+++ sripts/MoMoK/MoMoK_trees.R
@@ -5,8 +5,26 @@
 
 # ----- 0. SETUP ---------------------------------------------------------------
 # ----- 0.1. Packages  ---------------------------------------------------------
+# install.packages("usethis")
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("here")
+install.packages("tibble")
+install.packages("stargazer")
+install.packages("dplyr")
+
 # ----- 0.2. library   ---------------------------------------------------------
+library("usethis")
+library("tidyverse")
+library("ggplot2")
+library("here")
+library("tibble")
+library("dplyr")
+library("stargazer")
+
 # ----- 0.3. working directory -------------------------------------------------
+here::here()
+getwd()
 
 # ----- 1. DATA ----------------------------------------------------------------
 # ----- 1.1. import ------------------------------------------------------------
